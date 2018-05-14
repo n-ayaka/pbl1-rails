eb237a49fcb225e00022122409014a9d5c593073
n-ayaka <kd1188211@st.kobedenshi.ac.jp>
2018-05-14 10:59:06 +0900

    Update atts_controller.rb

--- app/controllers/atts_controller.rb
+++ app/controllers/atts_controller.rb
@@ -1,11 +1,11 @@
 class AttsController < ApplicationController
   def index
   	@students = Attendance.joins(:user).select("attendances.*,users.*")
-  	render json: @students
+  	# render json: @students
   end
 
   def show
   	@atts = Attendance.joins(:user).select("attendances.*,users.*").where(id: params[:id])
-  	render json: @atts
+  	# render json: @atts
   end
 end
